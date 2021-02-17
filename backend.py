f54bcd84ada69aeb4fc5016737e59bf3a6fc6fc5
Neil M <n.mckenzie@live.com>
2021-02-17 11:23:25 -0600

    first commit

---
+++ backend.py
@@ -0,0 +1,80 @@
+import mysql.connector
+def connect():
+    con = mysql.connector.connect(
+        user="root", 
+        password = "Food414288!", 
+        host="localhost", 
+        database = "booksdb"
+    )
+    cursor = con.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY AUTO_INCREMENT, title text, author text, year integer, isbn integer)")
+    con.commit()
+    con.close()
+
+def insert(title, author, year, isbn):
+    con = mysql.connector.connect(
+        user="root", 
+        password = "Food414288!", 
+        host="localhost", 
+        database = "booksdb"
+    )
+    cursor = con.cursor()
+    cursor.execute("INSERT INTO book (title,author,year,isbn) VALUES (%s,%s,%s,%s)",(title, author, year, isbn))
+    con.commit()
+    con.close()
+
+def view():
+    con = mysql.connector.connect(
+        user="root", 
+        password = "Food414288!", 
+        host="localhost", 
+        database = "booksdb"
+    )
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM book")
+    rows=cursor.fetchall()
+    con.close()
+    return rows
+
+def search(title="",author="",year="",isbn=""):
+    con = mysql.connector.connect(
+        user="root", 
+        password = "Food414288!", 
+        host="localhost", 
+        database = "booksdb"
+    )
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM book WHERE title=%s OR author=%s OR year=%s OR isbn=%s", (title,author,year,isbn))
+    rows=cursor.fetchall()
+    con.close()
+    return rows
+
+def delete(idnum):
+    con = mysql.connector.connect(
+        user="root", 
+        password = "Food414288!", 
+        host="localhost", 
+        database = "booksdb"
+    )
+    cursor = con.cursor()
+    cursor.execute("DELETE FROM book WHERE id=%s", (idnum,))
+    con.commit()
+    con.close()
+
+def update(title, author, year, isbn, idnum):
+    con = mysql.connector.connect(
+        user="root", 
+        password = "Food414288!", 
+        host="localhost", 
+        database = "booksdb"
+    )
+    cursor = con.cursor()
+    cursor.execute("UPDATE book SET title=%s, author=%s, year=%s, isbn=%s WHERE id=%s", (title,author,year,isbn, idnum))
+    con.commit()
+    con.close()
+
+connect()
+update('The moon', 'Neil', 1988, 696969, 5)
+
+print(view())
+print(search(author='John Smith'))
